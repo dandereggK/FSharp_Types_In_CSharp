9880038d5fb289ae3f8449ef8de2dfd25937d327
Daniel Anderegg <DAnderegg@karmak.com>
2019-11-29 19:35:27 -0600

    Start with models

---
+++ Models.fs
@@ -0,0 +1,18 @@
+﻿namespace Example
+
+type Cat = 
+  { Name : string 
+    Age : string
+    Lives : int }
+
+
+[<Struct>]
+type Date =
+  { Year : int
+    Month : uint8
+    Day : uint8 }
+
+type Dog =
+  { Name : string 
+    mutable LastThingEaten : string }
+
---
+++ Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Example
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
